3fc7de21bcc284b4d6c0ea41691e9cfc7889b8e6
Erwin-Cino <ErwinCino0812@gmail.com>
2019-12-04 22:44:06 +1100

    fix: Error of a comma

--- app.js
+++ app.js
@@ -5,33 +5,52 @@ const { buildSchema } = require("graphql");
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use(
   "/graphql",
   graphqlHttp({
     schema: buildSchema(`
+        type Event {
+          _id: ID!
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+        input EventInput {
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
-
         type RootMutation {
-            createEvent(title: String): String
+            createEvent(eventInput: EventInput): Event
         }
-
         schema {
             query: RootQuery
             mutation: RootMutation
         }
     `),
     rootValue: {
-      //resolver
       events: () => {
-        return ["Romantic Cooking", "Nude painting", "Hackathon"];
+        return events;
       },
       createEvent: args => {
-        const eventTitle = args.title;
-        return eventTitle;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date
+        };
+        events.push(event);
+        return event;
       }
     },
     graphiql: true
